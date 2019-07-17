f12722b72c06d356af15980d2e7cb6b0c63bcab1
Jaroslav Bažant <bazanjar@fit.cvut.cz>
2019-07-17 11:49:20 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Jaroslav Bažant on 17/07/2019.
+//  Copyright © 2019 Jaroslav Bažant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
